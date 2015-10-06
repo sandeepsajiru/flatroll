3270334fdf43b3bee0442d542af35c830999614e
Sandeep Wadhwa <sandeep.sajiru@gmail.com>
2015-10-06 22:39:48 +0530

    Updated the integrated code with discussion in Meeting-5.  It is fully functional with DB Updations as well.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
-var logger = require('morgan');
 var bodyParser = require('body-parser');
+var logger = require('morgan');
 
 // Mongoose import
 
@@ -64,15 +64,22 @@ var flatSchema = new Schema(
 
 var receiptsSchema = new Schema(
 {
+    paymentItems:'string',
+    paymentTypes:'string',
     id:'string',
+    idEditorEnabled: 'string',
     receiptId: 'string',
+    receiptIdEditorEnabled: 'string',
     amount: 'string',
-    date: 'date',
+    amountEditorEnabled: 'string',
+    date: 'string',
+    dateEditorEnabled: 'string',
     paymentMode: 'string',
     paymentTyp: 'string',
     remarks: 'string',
+    remarksEditorEnabled: 'string',
     received: 'string',
-    isValid: 'Boolean'
+    receivedEditorEnabled: 'string'    
 });
 // Mongoose Model definition
 
@@ -86,11 +93,11 @@ var flats = mongoose.model('flat', flatSchema);
 var receipts = mongoose.model('receipt', receiptsSchema);
 
 // Bootstrap express
-
 var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 
 var server = app.listen(8081, function () {
 
@@ -101,18 +108,102 @@ var server = app.listen(8081, function () {
 
 // URLS management
 
+app.put('/flat/:id',(function(req,res){
+  flats.findOne({ _id: req.params.id }, function(err, flat) {
+    if (err) {
+      return res.send(err);
+    }
+
+    for (prop in req.body) {
+      flat[prop] = req.body[prop];
+    }
+
+    // save the flat
+    flat.save(function(err) {
+      if (err) {
+        return res.send(err);
+      }
+
+      res.json({ message: 'flat updated!' });
+    });
+  });
+}));    
+
+app.put('/owner/:id',(function(req,res){
+  owners.findOne({ _id: req.params.id }, function(err, owner) {
+    if (err) {
+      return res.send(err);
+    }
+
+    for (prop in req.body) {
+      owner[prop] = req.body[prop];
+    }
+
+    // save the flat
+    owner.save(function(err) {
+      if (err) {
+        return res.send(err);
+      }
+
+      res.json({ message: 'owner updated!' });
+    });
+  });
+}));         
+
+app.put('/tenant/:id',(function(req,res){
+	
+	/*
+	tenants.findAndModify({ _id: req.params.id }, function(err, tenant)
+		{
+			if (err) {
+				return res.send(err);
+			}
+			res.json({ message: 'tenant updated!' });
+		});
+	*/
+	console.log('Req_ID: '+req.params.id);
+	tenants.findOne({ _id: req.params.id }, function(err, tenant) {
+    if (err) {
+      return res.send(err);
+    }
+
+    for (prop in req.body) {
+      tenant[prop] = req.body[prop];
+	  console.log(req.body[prop]);
+    }
+
+    // save the tenant
+    tenant.save(function(err) {
+      if (err) {
+		  console.log(err);
+        return res.send(err);
+      }
+
+      res.json({ message: 'tenant updated!' });
+    });
+  });
+  
+})); 
+    
 app.post('/',function(req, res){
 
 		var Receipt = new receipts({
+        paymentItems : req.body.paymentItems,
+        paymentTypes : req.body.paymentTypes,
         id : req.body.id,
+        idEditorEnabled  : req.body.idEditorEnabled ,
         receiptId : req.body.receiptId ,
+        receiptIdEditorEnabled : req.body.receiptIdEditorEnabled ,
         amount : req.body.amount ,
+        amountEditorEnabled : req.body.amountEditorEnabled ,
         date : req.body.date ,
+        dateEditorEnabled : req.body.dateEditorEnabled ,
         paymentMode: req.body.paymentMode,
         paymentTyp: req.body.paymentTyp,
         remarks: req.body.remarks ,
-        received: req.body.received,
-        isValid: false
+        remarksEditorEnabled: req.body.remarksEditorEnabled ,
+        received: req.body.received ,
+        receivedEditorEnabled: req.body.receivedEditorEnabled
         });
 		Receipt.save(function(err, Receipt) {
 			if (err){
@@ -121,7 +212,7 @@ app.post('/',function(req, res){
 			return res.json(Receipt);
 		});
         console.log(Receipt)
-	})
+	});
 
 app.get('/tenantDetails', function (req, res) {
     tenants.find({}, function (err, docs) {
@@ -136,27 +227,38 @@ app.get('/ownerDetails', function (req, res) {
 });
 
 app.get('/flatDetails', function (req, res) {
+    console.log('Got Get Call');
     flats.find({}, function (err, docs) {
         res.json({ docs: docs });
     });
 });
 
 app.get('/receiptDetails', function (req, res) {
+    console.log('Got Get Call');
     receipts.find({}, function (err, docs) {
         res.json({ docs: docs });
     });
 });
 
 
-app.get('/dashboard', function (req, res) {
+app.get('/tenants', function (req, res) {
     res.sendFile(__dirname + '/dashboard.html');
 });
 
+app.get('/owners', function (req, res) {
+    res.sendFile(__dirname + '/dashboard.html');
+});
 
+app.get('/flats', function (req, res) {
+    res.sendFile(__dirname + '/dashboard.html');
 });
 
 app.get('/receipts', function (req, res) {
     res.sendFile(__dirname + '/receipts.html');
 });
 
-	
+app.get('/dashboard', function (req, res) {
+    res.sendFile(__dirname + '/dashboard.html');
+});
+
+});
